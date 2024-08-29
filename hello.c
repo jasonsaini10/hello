985400fe54633c619752be4a692102398d77060c
Jason Saini <jsaini2@huskers.unl.edu>
2024-08-29 09:57:32 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jason Saini
+ * Date: 2024/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jason Saini\n");
+
+  return 0;
+}
